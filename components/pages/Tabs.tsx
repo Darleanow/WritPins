eacef072e6e9a8ccf9604f979aa1fcbdefe220df
Enzo Hugonnier <enzohugonn@gmail.com>
2024-07-29 22:57:59 +0200

    fix(Pages): Comment out dead code

--- components/pages/Tabs.tsx
+++ components/pages/Tabs.tsx
@@ -12,7 +12,7 @@ import { cog, flash, list } from 'ionicons/icons';
 
 import Home from './Feed';
 import Lists from './Lists';
-import ListDetail from './ListDetail';
+// import ListDetail from './ListDetail';
 import Settings from './Settings';
 
 const Tabs = () => {
@@ -21,7 +21,7 @@ const Tabs = () => {
       <IonRouterOutlet>
         <Route path="/feed" render={() => <Home />} exact={true} />
         <Route path="/lists" render={() => <Lists />} exact={true} />
-        <Route path="/lists/:listId" render={() => <ListDetail />} exact={true} />
+        {/* <Route path="/lists/:listId" render={() => <ListDetail />} exact={true} /> */}
         <Route path="/settings" render={() => <Settings />} exact={true} />
         <Route path="" render={() => <Redirect to="/feed" />} exact={true} />
       </IonRouterOutlet>
